277eb548ea6064daf11d4bfbbb1c1b7f5e181421
SamihaD <samiha.dr@gmail.com>
2022-11-18 16:13:29 +0100

    Bomberman

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.anc3_2122_projet_g01 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    //opens com.example.anc3_2122_projet_g01 to javafx.fxml;
+    //exports com.example.anc3_2122_projet_g01;
+}
